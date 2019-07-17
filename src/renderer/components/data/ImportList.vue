df8fc978d8935da8849208d7a93ce0386107797b
GooGee <mou-gj@live.com>
2019-07-17 13:16:29 +0800

    add ModuleMenu

--- src/renderer/components/data/ImportList.vue
+++ src/renderer/components/data/ImportList.vue
@@ -10,7 +10,7 @@
 <script>
     import builder from '@/model/builder'
     import { look, sure } from '@/model/ui/Dialogue'
-    import Menu from '@/model/ui/Menu'
+    import ModuleMenu from '@/model/ui/ModuleMenu'
     import Import from './Import'
 
     export default {
@@ -23,38 +23,17 @@
         },
         methods: {
             add() {
-                let menu = new Menu()
+                const menu = new ModuleMenu(builder)
 
-                let list = builder.project.ModuleManager.list
-                list.forEach(mmm => {
-                    menu.add(mmm.name, label => {
-                        try {
-                            let iii = this.cmodule.ImportManager.make(mmm.path)
-                            this.cmodule.ImportManager.add(iii)
-                            this.cmodule.save()
-                        } catch (error) {
-                            look(error, 400)
-                        }
-                    })
+                menu.show(path => {
+                    try {
+                        const iii = this.cmodule.ImportManager.make(path)
+                        this.cmodule.ImportManager.add(iii)
+                        this.cmodule.save()
+                    } catch (error) {
+                        look(error, 400)
+                    }
                 })
-
-                menu.addSeparator()
-
-                let aml = builder.project.AmbientModuleList
-                aml.forEach(mmm => {
-                    let name = mmm.name.slice(1, -1)
-                    menu.add(name, label => {
-                        try {
-                            let iii = this.cmodule.ImportManager.make(label)
-                            this.cmodule.ImportManager.add(iii)
-                            this.cmodule.save()
-                        } catch (error) {
-                            look(error, 400)
-                        }
-                    })
-                })
-
-                menu.show()
             },
             remove(cimport) {
                 sure('Are you sure?').then(result => {
---
+++ src/renderer/model/ui/ModuleMenu.ts
@@ -0,0 +1,46 @@
+import * as path from 'path'
+import Menu, { CallBack } from "./Menu"
+import { Builder } from "../Builder"
+
+export default class ModuleMenu {
+    builder: Builder
+
+    constructor(builder: Builder) {
+        this.builder = builder
+    }
+
+    get project() {
+        return this.builder.project!
+    }
+
+    get module() {
+        return this.builder.module!
+    }
+
+    show(CallBack: CallBack) {
+        const menu = new Menu()
+
+        const list = this.project.ModuleManager.list
+        list.forEach(mmm => {
+            if (this.module.path == mmm.path) {
+                return
+            }
+            menu.add(mmm.name, label => {
+                // not working
+                const folder = path.dirname(this.module.path)
+                const relativePath = path.relative(folder, mmm.path)
+                CallBack(relativePath)
+            })
+        })
+
+        menu.addSeparator()
+
+        const aml = this.project.AmbientModuleList
+        aml.forEach(mmm => {
+            const name = mmm.name.slice(1, -1) // trim quote
+            menu.add(name, label => CallBack(label))
+        })
+
+        menu.show()
+    }
+}
