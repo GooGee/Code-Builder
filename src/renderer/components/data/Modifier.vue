7ec0c9e3b197115a31fbd92eab2a06effe7b1fea
GooGee <mou-gj@live.com>
2019-07-17 13:29:07 +0800

    change Modifier label

--- src/renderer/components/data/Modifier.vue
+++ src/renderer/components/data/Modifier.vue
@@ -1,16 +1,16 @@
 <template>
     <span>
         <label v-if="kind=='abstract'" class="syntax">
-            <input v-model="modifier.hasAbstract" type="checkbox"> abstract
+            <input v-model="modifier.hasAbstract" type="checkbox"> Abstract
         </label>
         <label v-if="kind=='export'" class="syntax">
-            <input v-model="modifier.hasExport" type="checkbox"> export
+            <input v-model="modifier.hasExport" type="checkbox"> Export
         </label>
         <label v-if="kind=='readonly'" class="syntax">
-            <input v-model="modifier.hasReadonly" type="checkbox"> readonly
+            <input v-model="modifier.hasReadonly" type="checkbox"> ReadOnly
         </label>
         <label v-if="kind=='static'" class="syntax">
-            <input v-model="modifier.hasStatic" type="checkbox"> static
+            <input v-model="modifier.hasStatic" type="checkbox"> Static
         </label>
     </span>
 </template>
