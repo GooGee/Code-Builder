97472b811bd964edf3e59d454e9b368be49e4e63
GooGee <mou-gj@live.com>
2019-07-20 19:55:37 +0800

    add TypeList

---
+++ src/renderer/components/data/TypeList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div v-for="type in manager.list">
+            <span @click="remove(type)" class="button"> - </span>
+            <TypeBox :box="type" kind="Type"></TypeBox>
+        </div>
+    </div>
+</template>
+
+<script>
+    import TypeBox from './TypeBox'
+
+    export default {
+        name: 'TypeList',
+        components: { TypeBox },
+        props: ['manager'],
+        data() {
+            return {
+            }
+        },
+        methods: {
+            remove() {
+
+            }
+        }
+    }
+</script>
