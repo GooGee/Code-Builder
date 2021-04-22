bf264bb0db349930e1f76e93b67313e28cc853c2
GooGee <mou-gj@live.com>
2021-04-22 17:54:15 +0800

    -+ App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="text-center">
+            <img src="logo.svg" alt="logo" height="333px" />
+            <h1>Code Builder</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
