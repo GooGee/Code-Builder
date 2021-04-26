96ec5221c272a24406c559405e297c3a6ad5056a
GooGee <mou-gj@live.com>
2021-04-26 17:22:21 +0800

    + Host.ts

---
+++ src/model/TS/Host.ts
@@ -0,0 +1,36 @@
+import ts from 'typescript'
+import LanguageServiceHost from './LanguageServiceHost'
+
+export default class Host extends LanguageServiceHost {
+    readonly scriptVersionMap: Map<string, string> = new Map()
+
+    constructor(
+        fs: ts.System,
+        compilerOptions: ts.CompilerOptions,
+        files: string[],
+    ) {
+        super(fs, compilerOptions)
+
+        files.forEach((file) => {
+            this.scriptVersionMap.set(file, '1')
+        })
+    }
+
+    getScriptFileNames(): string[] {
+        return this.fs.readDirectory(this.fs.getCurrentDirectory())
+    }
+
+    getScriptVersion(fileName: string): string {
+        return this.scriptVersionMap.get(fileName) ?? '0'
+    }
+
+    getScriptSnapshot(fileName: string): ts.IScriptSnapshot | undefined {
+        if (this.scriptVersionMap.has(fileName)) {
+            const content = this.fs.readFile(fileName)
+            if (content) {
+                return ts.ScriptSnapshot.fromString(content)
+            }
+        }
+        return
+    }
+}
