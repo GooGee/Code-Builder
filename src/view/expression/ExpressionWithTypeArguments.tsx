2d4c1bc538d0f704e37dd5d3faebfb82004c5f50
GooGee <mou-gj@live.com>
2021-04-29 18:22:33 +0800

    + ExpressionWithTypeArguments.tsx

---
+++ src/view/expression/ExpressionWithTypeArguments.tsx
@@ -0,0 +1,19 @@
+import React, { ReactElement } from 'react'
+import ts from 'typescript'
+import TypeArgumentxx from '../type/TypeArgumentxx'
+import Expression from './Expression'
+
+interface Props {
+    node: ts.ExpressionWithTypeArguments
+}
+
+export default function ExpressionWithTypeArguments({
+    node,
+}: Props): ReactElement {
+    return (
+        <span>
+            <Expression node={node.expression}></Expression>
+            <TypeArgumentxx list={node.typeArguments}></TypeArgumentxx>
+        </span>
+    )
+}
