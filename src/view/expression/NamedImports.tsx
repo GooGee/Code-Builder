31f01978b6a938aa11ef31fc469d3b52cb2fa031
GooGee <mou-gj@live.com>
2021-04-29 18:23:39 +0800

    + NamedImports.tsx

---
+++ src/view/expression/NamedImports.tsx
@@ -0,0 +1,23 @@
+import React, { ReactElement } from 'react'
+import ts from 'typescript'
+import UniqueKey from '../../helper/UniqueKey'
+import ImportSpecifier from './ImportSpecifier'
+
+interface Props {
+    node: ts.NamedImports
+}
+
+export default function NamedImports({ node }: Props): ReactElement {
+    const uk = UniqueKey()
+    return (
+        <span>
+            {'{ '}
+            {node.elements.map((item) => {
+                return (
+                    <ImportSpecifier node={item} key={uk()}></ImportSpecifier>
+                )
+            })}
+            {' }'}
+        </span>
+    )
+}
