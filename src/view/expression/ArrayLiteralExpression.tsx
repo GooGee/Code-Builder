1db2450ff01c79e82e4aea3a3ca7a52e2680b93c
GooGee <mou-gj@live.com>
2021-04-29 18:20:55 +0800

    + ArrayLiteralExpression.tsx

---
+++ src/view/expression/ArrayLiteralExpression.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from 'react'
+import ts from 'typescript'
+import Expressionxx from './Expressionxx'
+
+interface Props {
+    node: ts.ArrayLiteralExpression
+}
+
+export default function ArrayLiteralExpression({ node }: Props): ReactElement {
+    return (
+        <span>
+            [<Expressionxx list={node.elements}></Expressionxx>]
+        </span>
+    )
+}
