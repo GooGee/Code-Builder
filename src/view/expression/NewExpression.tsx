0fe7cb7eea73b5fb07c97e04465591aea8c18a33
GooGee <mou-gj@live.com>
2021-04-29 18:24:01 +0800

    + NewExpression.tsx

---
+++ src/view/expression/NewExpression.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement } from 'react'
+import ts from 'typescript'
+import Keyword from '../text/Keyword'
+import CallExpression from './CallExpression'
+
+interface Props {
+    node: ts.NewExpression
+}
+
+export default function NewExpression({ node }: Props): ReactElement {
+    return (
+        <>
+            <Keyword kind={node.kind}></Keyword>{' '}
+            <CallExpression node={node}></CallExpression>
+        </>
+    )
+}
