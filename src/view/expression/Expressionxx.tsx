8aa9e5e64766542837112fbe118d8e2560de918e
GooGee <mou-gj@live.com>
2021-04-29 18:22:43 +0800

    + Expressionxx.tsx

---
+++ src/view/expression/Expressionxx.tsx
@@ -0,0 +1,33 @@
+import React, { ReactElement } from 'react'
+import ts from 'typescript'
+import UniqueKey from '../../helper/UniqueKey'
+import ExpressionRoot from './ExpressionRoot'
+
+interface Props {
+    list: ts.NodeArray<ts.Expression>
+}
+
+export default function Expressionxx({ list }: Props): ReactElement | null {
+    if (list === undefined) {
+        return null
+    }
+
+    if (list.length === 0) {
+        return null
+    }
+
+    const uk = UniqueKey()
+    return (
+        <span>
+            {list
+                .map((item) => {
+                    return (
+                        <ExpressionRoot node={item} key={uk()}></ExpressionRoot>
+                    )
+                })
+                .reduce((previousValue, currentValue): any => {
+                    return [previousValue, ', ', currentValue]
+                })}
+        </span>
+    )
+}
