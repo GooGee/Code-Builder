780917cee34bd82e108dc1dca1d9203d266f5556
GooGee <mou-gj@live.com>
2021-04-29 18:22:55 +0800

    + Identifier.tsx

---
+++ src/view/expression/Identifier.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react'
+import ts from 'typescript'
+
+interface Props {
+    node: ts.Identifier | ts.PrivateIdentifier
+}
+
+export default function Identifier({ node }: Props): ReactElement {
+    return <span className="identifier">{node.text}</span>
+}
