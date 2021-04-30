7b84aa0304eff4fc1c2bf804ed1c639828df2cd1
GooGee <mou-gj@live.com>
2021-04-30 14:27:23 +0800

    + TypeReference.tsx

---
+++ src/view/type/TypeReference.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement } from 'react'
+import ts from 'typescript'
+import EntityName from '../expression/EntityName'
+import TypeArgumentxx from './TypeArgumentxx'
+
+interface Props {
+    node: ts.TypeReferenceNode
+}
+
+export default function TypeReference({ node }: Props): ReactElement {
+    return (
+        <span>
+            <EntityName node={node.typeName}></EntityName>
+            <TypeArgumentxx list={node.typeArguments}></TypeArgumentxx>
+        </span>
+    )
+}
