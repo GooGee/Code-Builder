72c241fb48cc9d831f72d5c2a6ddb26c583af1e6
GooGee <mou-gj@live.com>
2021-05-02 11:31:21 +0800

    + ForOfStatement.tsx

---
+++ src/view/statement/ForOfStatement.tsx
@@ -0,0 +1,28 @@
+import React, { ReactElement } from 'react'
+import ts from 'typescript'
+import Block from '../block/Block'
+import VariableDeclarationList from '../declaration/VariableDeclarationList'
+import ExpressionRoot from '../expression/ExpressionRoot'
+import Keyword from '../text/Keyword'
+
+interface Props {
+    node: ts.ForOfStatement
+}
+
+export default function ForOfStatement({ node }: Props): ReactElement {
+    return (
+        <div>
+            <div>
+                <Keyword kind={node.kind}></Keyword>{' '}
+                <VariableDeclarationList
+                    node={node.initializer as any}
+                ></VariableDeclarationList>
+            </div>
+            <div className="block-padding">
+                <span className="keyword">of </span>
+                <ExpressionRoot node={node.expression}></ExpressionRoot>
+            </div>
+            <Block node={node.statement as any}></Block>
+        </div>
+    )
+}
