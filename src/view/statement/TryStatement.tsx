07db19b5b92d98fade95b4aa70281d1a805454c8
GooGee <mou-gj@live.com>
2021-05-02 11:32:45 +0800

    + TryStatement.tsx

---
+++ src/view/statement/TryStatement.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement } from 'react'
+import ts from 'typescript'
+import Block from '../block/Block'
+import CatchClause from '../block/CatchClause'
+import Keyword from '../text/Keyword'
+
+interface Props {
+    node: ts.TryStatement
+}
+
+export default function TryStatement({ node }: Props): ReactElement {
+    return (
+        <div>
+            <Keyword kind={node.kind}></Keyword>
+            <Block node={node.tryBlock}></Block>
+            <CatchClause node={node.catchClause!}></CatchClause>
+            <div className="keyword">finally</div>
+            <Block node={node.finallyBlock}></Block>
+        </div>
+    )
+}
