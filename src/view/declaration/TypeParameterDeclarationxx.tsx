c74c9d8873438bb508c759d094432416167529e8
GooGee <mou-gj@live.com>
2021-05-03 11:44:26 +0800

    + TypeParameterDeclarationxx.tsx

---
+++ src/view/declaration/TypeParameterDeclarationxx.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement } from 'react'
+import ts from 'typescript'
+import UniqueKey from '../../helper/UniqueKey'
+import TypeParameterDeclaration from './TypeParameterDeclaration'
+
+interface Props {
+    list: ts.NodeArray<ts.TypeParameterDeclaration> | undefined
+}
+
+export default function TypeParameterDeclarationxx({
+    list,
+}: Props): ReactElement | null {
+    if (list === undefined) {
+        return null
+    }
+    if (list.length === 0) {
+        return null
+    }
+
+    const uk = UniqueKey()
+    function getxx(lll: ts.NodeArray<ts.TypeParameterDeclaration>) {
+        if (lll.length === 0) {
+            return null
+        }
+        return lll
+            .map((item) => {
+                return (
+                    <TypeParameterDeclaration
+                        node={item}
+                        key={uk()}
+                    ></TypeParameterDeclaration>
+                )
+            })
+            .reduce((previousValue, currentValue): any => {
+                return [previousValue, ', ', currentValue]
+            })
+    }
+    return <span>&lt;{getxx(list)}&gt;</span>
+}
