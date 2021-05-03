5dc3f69189b616c8e93eac8d11c99bfd6310e04e
GooGee <mou-gj@live.com>
2021-05-03 11:44:52 +0800

    + VariableDeclarationxx.tsx

---
+++ src/view/declaration/VariableDeclarationxx.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement } from 'react'
+import ts from 'typescript'
+import UniqueKey from '../../helper/UniqueKey'
+import VariableDeclaration from './VariableDeclaration'
+
+interface Props {
+    list: ts.NodeArray<ts.VariableDeclaration>
+}
+
+export default function VariableDeclarationxx({ list }: Props): ReactElement {
+    const uk = UniqueKey()
+    return (
+        <span>
+            {list.map((item) => {
+                return (
+                    <VariableDeclaration
+                        node={item}
+                        key={uk()}
+                    ></VariableDeclaration>
+                )
+            })}
+        </span>
+    )
+}
