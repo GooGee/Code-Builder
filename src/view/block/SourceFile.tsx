6f5561ea0a09203fb6db3894d90c66cfa807fc6d
GooGee <mou-gj@live.com>
2021-05-11 15:37:08 +0800

    + LineButton in SourceFile.tsx

--- src/view/block/SourceFile.tsx
+++ src/view/block/SourceFile.tsx
@@ -1,35 +1,44 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import ts from 'typescript'
+import SourceFileMenuFactory from '../../helper/Menu/SourceFileMenuFactory'
 import UniqueKey from '../../helper/UniqueKey'
+import SourceFileContext, { ContextData } from '../context/SourceFileContext'
+import LineButton from '../control/LineButton'
 import Declaration from '../declaration/Declaration'
 
 interface Props {
-    sf: ts.SourceFile | undefined
+    sf: ts.SourceFile
 }
 
 export default function SourceFile({ sf }: Props): ReactElement | null {
-    if (sf === undefined) {
-        return null
-    }
-    if (sf.statements.length === 0) {
-        return null
+    const [state, setSourceFile] = useState(sf)
+    function update(sf: ts.SourceFile) {
+        setSourceFile(sf)
     }
 
     const uk = UniqueKey()
+    function getList(sf: ts.SourceFile) {
+        if (sf.statements.length === 0) {
+            return null
+        }
+        return sf.statements
+            .map((item) => (
+                <Declaration node={item as any} key={uk()}></Declaration>
+            ))
+            .reduce((previousValue, currentValue): any => [
+                previousValue,
+                <hr key={uk()} />,
+                currentValue,
+            ])
+    }
+
+    const data = new ContextData(state, update)
     return (
-        <div>
-            {sf.statements
-                .map((item) => {
-                    return (
-                        <Declaration
-                            node={item as any}
-                            key={uk()}
-                        ></Declaration>
-                    )
-                })
-                .reduce((previousValue, currentValue): any => {
-                    return [previousValue, <hr key={uk()} />, currentValue]
-                })}
-        </div>
+        <SourceFileContext.Provider value={data}>
+            {getList(state)}
+            <div>
+                <LineButton factory={SourceFileMenuFactory()}></LineButton>
+            </div>
+        </SourceFileContext.Provider>
     )
 }
