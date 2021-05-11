e73a82d53e70f2fec1ad05ced863f4ad54c273f0
GooGee <mou-gj@live.com>
2021-05-11 15:32:01 +0800

    + InterfaceTransformer.ts

---
+++ src/helper/Transformer/InterfaceTransformer.ts
@@ -0,0 +1,37 @@
+import ts from 'typescript'
+import Transformer from './Transformer'
+
+function addNote(
+    parent: ts.InterfaceDeclaration,
+    item: ts.TypeElement,
+    at?: ts.TypeElement,
+) {
+    const list = Transformer.insert<ts.TypeElement>(parent.members, item, at)
+    update(parent, list)
+}
+
+function deleteNote(parent: ts.InterfaceDeclaration, item: ts.TypeElement) {
+    const list = Transformer.remove<ts.TypeElement>(parent.members, item)
+    update(parent, list)
+}
+
+function update(
+    parent: ts.InterfaceDeclaration,
+    list: ts.NodeArray<ts.TypeElement>,
+) {
+    const to = ts.factory.updateInterfaceDeclaration(
+        parent,
+        parent.decorators,
+        parent.modifiers,
+        parent.name,
+        parent.typeParameters,
+        parent.heritageClauses,
+        list,
+    )
+    Transformer.transform(parent, to)
+}
+
+export default {
+    addNote,
+    deleteNote,
+}
