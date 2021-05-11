caa2e187b5384635d4277965fd2f155a7ebe5b96
GooGee <mou-gj@live.com>
2021-05-11 15:15:17 +0800

    + LineButton in EnumDeclaration.tsx

--- src/view/declaration/EnumDeclaration.tsx
+++ src/view/declaration/EnumDeclaration.tsx
@@ -1,6 +1,9 @@
 import React, { ReactElement } from 'react'
 import ts from 'typescript'
+import EnumMenuFactory from '../../helper/Menu/EnumMenuFactory'
+import SourceFileMenuFactory from '../../helper/Menu/SourceFileMenuFactory'
 import UniqueKey from '../../helper/UniqueKey'
+import LineButton from '../control/LineButton'
 import Identifier from '../expression/Identifier'
 import Keyword from '../text/Keyword'
 import Modifierxx from '../text/Modifierxx'
@@ -15,6 +18,7 @@ export default function EnumDeclaration({ node }: Props): ReactElement {
     return (
         <div>
             <div>
+                <LineButton factory={SourceFileMenuFactory(node)}></LineButton>
                 <Modifierxx list={node.modifiers}></Modifierxx>{' '}
                 <Keyword kind={node.kind}></Keyword>{' '}
                 <Identifier node={node.name}></Identifier>
@@ -24,6 +28,7 @@ export default function EnumDeclaration({ node }: Props): ReactElement {
                 {node.members.map((member) => {
                     return <EnumMember node={member} key={uk()}></EnumMember>
                 })}
+                <LineButton factory={EnumMenuFactory(node)}></LineButton>
             </div>
             {'}'}
         </div>
--- src/view/declaration/EnumMember.tsx
+++ src/view/declaration/EnumMember.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement } from 'react'
 import ts from 'typescript'
+import EnumMenuFactory from '../../helper/Menu/EnumMenuFactory'
+import LineButton from '../control/LineButton'
 import ExpressionRoot from '../expression/ExpressionRoot'
 import Identifier from '../expression/Identifier'
 
@@ -10,6 +12,9 @@ interface Props {
 export default function EnumMember({ node }: Props): ReactElement {
     return (
         <div>
+            <LineButton
+                factory={EnumMenuFactory(node.parent as any, node)}
+            ></LineButton>
             <Identifier node={node.name as any}></Identifier>
             {node.initializer ? ' = ' : null}
             <ExpressionRoot node={node.initializer}></ExpressionRoot>
