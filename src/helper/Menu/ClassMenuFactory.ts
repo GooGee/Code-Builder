fdfa6cba1488a4c3969f26d21b43b4084ed30200
GooGee <mou-gj@live.com>
2021-05-11 15:32:39 +0800

    + ClassMenuFactory.ts

---
+++ src/helper/Menu/ClassMenuFactory.ts
@@ -0,0 +1,34 @@
+import ts from 'typescript'
+import * as DeclarationFactory from '../Factory/DeclarationFactory'
+import ClassTransformer from '../Transformer/ClassTransformer'
+import MenuFactory from './MenuFactory'
+
+export default function ClassMenuFactory(
+    parent: ts.ClassDeclaration,
+    at?: ts.ClassElement,
+) {
+    return () => {
+        console.log('ClassMenuFactory')
+        const menu = MenuFactory.makeMenu('')
+        if (at !== undefined) {
+            menu.list.push(
+                MenuFactory.makeMenu('Delete', () => {
+                    ClassTransformer.deleteNote(parent, at)
+                }),
+                MenuFactory.makeMenu('----', MenuFactory.nothing, true),
+            )
+        }
+
+        menu.list.push(
+            MenuFactory.makeMenu('+ method', () => {
+                const item = DeclarationFactory.makeMethod('MethodName')
+                ClassTransformer.addNote(parent, item, at)
+            }),
+            MenuFactory.makeMenu('+ property', () => {
+                const item = DeclarationFactory.makeProperty('PropertyName')
+                ClassTransformer.addNote(parent, item, at)
+            }),
+        )
+        return menu
+    }
+}
