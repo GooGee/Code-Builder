5790c5ebf1eded8388d636f9617fda85baf54317
GooGee <mou-gj@live.com>
2021-05-12 16:30:39 +0800

    + Builder.tsx

---
+++ src/view/Builder.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from 'react'
+import Vendor from '../model/Vendor'
+import SourceFile from './block/SourceFile'
+
+interface Props {
+    state: Vendor
+}
+
+export default function Builder({ state }: Props): ReactElement {
+    return (
+        <div>
+            <SourceFile sf={state.sf}></SourceFile>
+        </div>
+    )
+}
