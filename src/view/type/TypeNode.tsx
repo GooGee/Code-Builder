fc66b2dd5aaa2e7a725655061ee73ba0fa0531ab
GooGee <mou-gj@live.com>
2021-05-17 15:53:32 +0800

    + TypeRoot in TypeNode.tsx

--- src/view/type/TypeNode.tsx
+++ src/view/type/TypeNode.tsx
@@ -1,51 +1,65 @@
 import React, { ReactElement } from 'react'
 import ts from 'typescript'
+import KeywordTypeList from '../../asset/KeywordTypeList'
+import { UpdateTypeNode } from '../../helper/Menu/TypeMenuFactory'
 import ExpressionWithTypeArguments from '../expression/ExpressionWithTypeArguments'
 import ArrayType from './ArrayType'
 import KeywordType from './KeywordType'
+import LiteralType from './LiteralType'
 import TypeReference from './TypeReference'
+import TypeRoot from './TypeRoot'
 import UnionType from './UnionType'
 
 interface Props {
+    editing?: boolean
     node: ts.TypeNode | undefined
+    updateType?: UpdateTypeNode
 }
 
-export default function TypeNode({ node }: Props): ReactElement | null {
-    if (node === undefined) {
-        return null
+export default function TypeNode({
+    editing,
+    node,
+    updateType = (tn: ts.TypeNode) => {},
+}: Props): ReactElement | null {
+    function getNode() {
+        if (node === undefined) {
+            return null
+        }
+
+        if (ts.isArrayTypeNode(node)) {
+            return <ArrayType node={node}></ArrayType>
+        }
+
+        if (ts.isExpressionWithTypeArguments(node)) {
+            return (
+                <ExpressionWithTypeArguments
+                    node={node}
+                ></ExpressionWithTypeArguments>
+            )
+        }
+
+        if (ts.isLiteralTypeNode(node)) {
+            return <LiteralType node={node}></LiteralType>
+        }
+
+        if (ts.isTypeReferenceNode(node)) {
+            return <TypeReference node={node}></TypeReference>
+        }
+
+        if (ts.isUnionTypeNode(node)) {
+            return <UnionType node={node}></UnionType>
+        }
+
+        if (KeywordTypeList.includes(node.kind as any)) {
+            return <KeywordType node={node as any}></KeywordType>
+        }
+
+        throw new Error(`Error loading type: ${ts.SyntaxKind[node.kind]}`)
     }
 
-    if (ts.isArrayTypeNode(node)) {
-        return <ArrayType node={node}></ArrayType>
-    }
-
-    if (ts.isExpressionWithTypeArguments(node)) {
-        return (
-            <ExpressionWithTypeArguments
-                node={node}
-            ></ExpressionWithTypeArguments>
-        )
-    }
-
-    if (ts.isTypeReferenceNode(node)) {
-        return <TypeReference node={node}></TypeReference>
-    }
-
-    if (ts.isUnionTypeNode(node)) {
-        return <UnionType node={node}></UnionType>
-    }
-
-    const list = [
-        ts.SyntaxKind.BooleanKeyword,
-        ts.SyntaxKind.NullKeyword,
-        ts.SyntaxKind.NumberKeyword,
-        ts.SyntaxKind.StringKeyword,
-        ts.SyntaxKind.UndefinedKeyword,
-        ts.SyntaxKind.VoidKeyword,
-    ]
-    if (list.includes(node.kind)) {
-        return <KeywordType node={node as any}></KeywordType>
-    }
-
-    throw new Error(`Error loading type: ${ts.SyntaxKind[node.kind]}`)
+    return (
+        <TypeRoot editing={editing} node={node} updateType={updateType}>
+            {getNode()}
+        </TypeRoot>
+    )
 }
