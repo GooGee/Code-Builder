79a8897d872d8e3b566e58d69354baf032f7d81b
GooGee <mou-gj@live.com>
2021-05-17 17:22:46 +0800

    + editingView in DeclarationLine.tsx

--- src/view/control/DeclarationLine.tsx
+++ src/view/control/DeclarationLine.tsx
@@ -1,24 +1,35 @@
 import React, { ReactElement, useState } from 'react'
 import Menu from '../../model/Menu'
-import LineButton from './LineButton'
+import MenuButton from './MenuButton'
+
+export interface Hide {
+    (): void
+}
 
 interface Props {
-    children?: Array<ReactElement | string>
+    children: Array<ReactElement | string>
+    editingView?: (hide: Hide) => ReactElement | null
     factory: () => Menu
 }
 
 export default function DeclarationLine({
     children,
+    editingView,
     factory,
 }: Props): ReactElement {
+    const [editing, setEditing] = useState(false)
     const [visible, setVisible] = useState(false)
     return (
         <div
             onMouseOver={() => setVisible(true)}
-            onMouseLeave={() => setVisible(false)}
+            onMouseLeave={() => (editing ? null : setVisible(false))}
         >
-            <LineButton visible={visible} factory={factory}></LineButton>
-            {children}
+            <MenuButton visible={visible} factory={factory}></MenuButton>
+            {editing ? (
+                editingView!(() => setEditing(false))
+            ) : (
+                <span onClick={() => setEditing(true)}>{children}</span>
+            )}
         </div>
     )
 }
