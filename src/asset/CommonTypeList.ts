148f987afc515871daab54fb070a583328dd7833
GooGee <mou-gj@live.com>
2021-05-19 15:50:36 +0800

    + CommonTypeList.ts

---
+++ src/asset/CommonTypeList.ts
@@ -0,0 +1,17 @@
+
+const CommonTypeList = [
+    'Array',
+    'Boolean',
+    'Date',
+    'Function',
+    'Generator',
+    'Iterable',
+    'Iterator',
+    'Map',
+    'Number',
+    'Object',
+    'Set',
+    'Symbol',
+]
+
+export default CommonTypeList
