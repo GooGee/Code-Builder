284620ab00c3894a12d847c049063566dd5e5708
GooGee <mou-gj@live.com>
2021-05-27 15:36:15 +0800

    + MenuButton in Block.tsx

--- src/view/block/Block.tsx
+++ src/view/block/Block.tsx
@@ -1,5 +1,8 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import ts from 'typescript'
+import StatementMenuFactory from '../../helper/Menu/StatementMenuFactory'
+import EditingBox from '../control/EditingBox'
+import MenuButton from '../control/MenuButton'
 import Statementxx from '../statement/Statementxx'
 
 interface Props {
@@ -13,16 +16,32 @@ export default function Block({
     prefix,
     suffix,
 }: Props): ReactElement | null {
+    const [editing, setEditing] = useState(false)
     if (node === undefined) {
         return null
     }
 
     return (
-        <div>
+        <div
+            onClick={(event) => {
+                event.stopPropagation()
+                setEditing(true)
+            }}
+        >
             {prefix}
             {'{'}
             <div className="pl-9">
                 <Statementxx list={node.statements}></Statementxx>
+                <div>
+                    {editing ? (
+                        <EditingBox hide={() => setEditing(false)}>
+                            <MenuButton
+                                visible={true}
+                                factory={StatementMenuFactory(node)}
+                            ></MenuButton>
+                        </EditingBox>
+                    ) : null}
+                </div>
             </div>
             {'}'}
             {suffix}
