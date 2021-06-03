fe45549fe367ee225c3ef04eb35d8df6c360a682
GooGee <mou-gj@live.com>
2021-06-03 22:44:29 +0800

    + IdentifierDeclaration in EnumDeclaration.tsx

--- src/view/declaration/EnumDeclaration.tsx
+++ src/view/declaration/EnumDeclaration.tsx
@@ -3,9 +3,9 @@ import ts from 'typescript'
 import EnumMenuFactory from '../../helper/Menu/EnumMenuFactory'
 import SourceFileMenuFactory from '../../helper/Menu/SourceFileMenuFactory'
 import UniqueKey from '../../helper/UniqueKey'
-import StatementLine from '../control/StatementLine'
 import MenuButton from '../control/MenuButton'
-import Identifier from '../expression/Identifier'
+import StatementLine from '../control/StatementLine'
+import IdentifierDeclaration from '../expression/IdentifierDeclaration'
 import Keyword from '../text/Keyword'
 import Modifierxx from '../text/Modifierxx'
 import EnumMember from './EnumMember'
@@ -18,17 +18,38 @@ export default function EnumDeclaration({ node }: Props): ReactElement {
     const uk = UniqueKey()
     return (
         <div>
-            <StatementLine menuFactory={SourceFileMenuFactory(node)}>
-                <Modifierxx list={node.modifiers}></Modifierxx>{' '}
-                <Keyword kind={node.kind}></Keyword>{' '}
-                <Identifier node={node.name}></Identifier>
-            </StatementLine>
+            <StatementLine
+                menuFactory={SourceFileMenuFactory(node)}
+                viewFactory={(editing) => (
+                    <span>
+                        <Modifierxx list={node.modifiers}></Modifierxx>{' '}
+                        <Keyword kind={node.kind}></Keyword>{' '}
+                        <IdentifierDeclaration
+                            editing={editing}
+                            node={node.name}
+                        ></IdentifierDeclaration>
+                    </span>
+                )}
+            ></StatementLine>
 
             {'{'}
             <div className="pl-9">
-                {node.members.map((member) => {
-                    return <EnumMember node={member} key={uk()}></EnumMember>
-                })}
+                {node.members.map((member) => (
+                    <StatementLine
+                        key={uk()}
+                        menuFactory={EnumMenuFactory(
+                            member.parent as any,
+                            member,
+                        )}
+                        viewFactory={(editing) => (
+                            <EnumMember
+                                editing={editing}
+                                node={member}
+                            ></EnumMember>
+                        )}
+                    ></StatementLine>
+                ))}
+
                 <MenuButton
                     visible={true}
                     factory={EnumMenuFactory(node)}
