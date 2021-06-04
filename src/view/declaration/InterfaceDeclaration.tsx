7a7c2df1de6b4b6bd26d8fbe03d2ec030eadc8be
GooGee <mou-gj@live.com>
2021-06-04 17:20:00 +0800

    + IdentifierDeclaration in InterfaceDeclaration.tsx

--- src/view/declaration/InterfaceDeclaration.tsx
+++ src/view/declaration/InterfaceDeclaration.tsx
@@ -3,12 +3,11 @@ import ts from 'typescript'
 import InterfaceMenuFactory from '../../helper/Menu/InterfaceMenuFactory'
 import SourceFileMenuFactory from '../../helper/Menu/SourceFileMenuFactory'
 import UniqueKey from '../../helper/UniqueKey'
-import StatementLine from '../control/StatementLine'
 import MenuButton from '../control/MenuButton'
-import Identifier from '../expression/Identifier'
+import StatementLine from '../control/StatementLine'
+import IdentifierDeclaration from '../expression/IdentifierDeclaration'
 import Keyword from '../text/Keyword'
 import Modifierxx from '../text/Modifierxx'
-import Heritagexx from './Heritagexx'
 import MethodSignature from './MethodSignature'
 import PropertySignature from './PropertySignature'
 import TypeParameterDeclarationxx from './TypeParameterDeclarationxx'
@@ -21,32 +20,47 @@ export default function InterfaceDeclaration({ node }: Props): ReactElement {
     const uk = UniqueKey()
     return (
         <div>
-            <StatementLine menuFactory={SourceFileMenuFactory(node)}>
-                <Modifierxx list={node.modifiers}></Modifierxx>{' '}
-                <Keyword kind={node.kind}></Keyword>{' '}
-                <Identifier node={node.name}></Identifier>
-                <TypeParameterDeclarationxx
-                    list={node.typeParameters}
-                ></TypeParameterDeclarationxx>
-            </StatementLine>
-
-            <Heritagexx list={node.heritageClauses}></Heritagexx>
+            <StatementLine
+                menuFactory={SourceFileMenuFactory(node)}
+                viewFactory={(editing) => (
+                    <span>
+                        <Modifierxx list={node.modifiers}></Modifierxx>{' '}
+                        <Keyword kind={node.kind}></Keyword>{' '}
+                        <IdentifierDeclaration
+                            editing={editing}
+                            node={node.name}
+                        ></IdentifierDeclaration>
+                        <TypeParameterDeclarationxx
+                            editing={editing}
+                            list={node.typeParameters}
+                        ></TypeParameterDeclarationxx>
+                    </span>
+                )}
+            ></StatementLine>
 
             {'{'}
             <div className="pl-9">
-                {node.members.map((item) =>
-                    ts.isMethodSignature(item) ? (
-                        <MethodSignature
-                            node={item}
-                            key={uk()}
-                        ></MethodSignature>
-                    ) : ts.isPropertySignature(item) ? (
-                        <PropertySignature
-                            node={item}
-                            key={uk()}
-                        ></PropertySignature>
-                    ) : null,
-                )}
+                {node.members.map((item) => (
+                    <StatementLine
+                        key={uk()}
+                        menuFactory={InterfaceMenuFactory(node, item)}
+                        viewFactory={(editing) => (
+                            <span>
+                                {ts.isMethodSignature(item) ? (
+                                    <MethodSignature
+                                        editing={editing}
+                                        node={item}
+                                    ></MethodSignature>
+                                ) : ts.isPropertySignature(item) ? (
+                                    <PropertySignature
+                                        editing={editing}
+                                        node={item}
+                                    ></PropertySignature>
+                                ) : null}
+                            </span>
+                        )}
+                    ></StatementLine>
+                ))}
                 <MenuButton
                     visible={true}
                     factory={InterfaceMenuFactory(node)}
