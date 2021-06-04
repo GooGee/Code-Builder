6582afa6c3123d718b20e20005243b2de1a9ec37
GooGee <mou-gj@live.com>
2021-06-04 17:39:56 +0800

    - Heritagexx in ClassDeclaration.tsx

--- src/view/declaration/ClassDeclaration.tsx
+++ src/view/declaration/ClassDeclaration.tsx
@@ -9,7 +9,6 @@ import IdentifierDeclaration from '../expression/IdentifierDeclaration'
 import Keyword from '../text/Keyword'
 import Modifierxx from '../text/Modifierxx'
 import ConstructorDeclaration from './ConstructorDeclaration'
-import Heritagexx from './Heritagexx'
 import MethodDeclaration from './MethodDeclaration'
 import PropertyDeclaration from './PropertyDeclaration'
 import TypeParameterDeclarationxx from './TypeParameterDeclarationxx'
@@ -22,39 +21,50 @@ export default function ClassDeclaration({ node }: Props): ReactElement {
     const uk = UniqueKey()
     return (
         <div>
-            <StatementLine menuFactory={SourceFileMenuFactory(node)}>
-                <Modifierxx list={node.modifiers}></Modifierxx>{' '}
-                <Keyword kind={node.kind}></Keyword>{' '}
-                <IdentifierDeclaration
-                    node={node.name!}
-                ></IdentifierDeclaration>
-                <TypeParameterDeclarationxx
-                    list={node.typeParameters}
-                ></TypeParameterDeclarationxx>
-            </StatementLine>
-
-            <Heritagexx list={node.heritageClauses}></Heritagexx>
+            <StatementLine
+                menuFactory={SourceFileMenuFactory(node)}
+                viewFactory={(editing) => (
+                    <span>
+                        <Modifierxx list={node.modifiers}></Modifierxx>{' '}
+                        <Keyword kind={node.kind}></Keyword>{' '}
+                        <IdentifierDeclaration
+                            editing={editing}
+                            node={node.name!}
+                        ></IdentifierDeclaration>
+                        <TypeParameterDeclarationxx
+                            editing={editing}
+                            list={node.typeParameters}
+                        ></TypeParameterDeclarationxx>
+                    </span>
+                )}
+            ></StatementLine>
 
             {'{'}
             <div className="pl-9">
-                {node.members.map((item) =>
-                    ts.isConstructorDeclaration(item) ? (
-                        <ConstructorDeclaration
-                            node={item}
-                            key={uk()}
-                        ></ConstructorDeclaration>
-                    ) : ts.isMethodDeclaration(item) ? (
-                        <MethodDeclaration
-                            node={item}
-                            key={uk()}
-                        ></MethodDeclaration>
-                    ) : ts.isPropertyDeclaration(item) ? (
-                        <PropertyDeclaration
-                            node={item}
-                            key={uk()}
-                        ></PropertyDeclaration>
-                    ) : null,
-                )}
+                {node.members.map((item) => (
+                    <StatementLine
+                        key={uk()}
+                        menuFactory={ClassMenuFactory(node, item)}
+                        viewFactory={(editing) =>
+                            ts.isConstructorDeclaration(item) ? (
+                                <ConstructorDeclaration
+                                    editing={editing}
+                                    node={item}
+                                ></ConstructorDeclaration>
+                            ) : ts.isMethodDeclaration(item) ? (
+                                <MethodDeclaration
+                                    editing={editing}
+                                    node={item}
+                                ></MethodDeclaration>
+                            ) : ts.isPropertyDeclaration(item) ? (
+                                <PropertyDeclaration
+                                    editing={editing}
+                                    node={item}
+                                ></PropertyDeclaration>
+                            ) : null
+                        }
+                    ></StatementLine>
+                ))}
                 <MenuButton
                     visible={true}
                     factory={ClassMenuFactory(node)}
