9f32be65b889a40b7cd4e9d624a6497438f0f8f2
GooGee <mou-gj@live.com>
2021-06-07 18:57:23 +0800

    + editing in Heritagexx.tsx

--- src/view/declaration/Heritagexx.tsx
+++ src/view/declaration/Heritagexx.tsx
@@ -3,18 +3,32 @@ import ts from 'typescript'
 import HeritageClause from '../block/HeritageClause'
 
 interface Props {
+    editing: boolean
     list: ts.NodeArray<ts.HeritageClause> | undefined
+    parent: ts.ClassDeclaration | ts.InterfaceDeclaration
 }
 
-export default function Heritagexx({ list }: Props): ReactElement | null {
+export default function Heritagexx({
+    editing,
+    list,
+    parent,
+}: Props): ReactElement | null {
     if (list === undefined) {
         return null
     }
 
     return (
         <div className="pl-9">
-            <HeritageClause node={list[0]} key="aaa"></HeritageClause>
-            <HeritageClause node={list[1]} key="bbb"></HeritageClause>
+            <HeritageClause
+                editing={editing}
+                node={list[0]}
+                key="aaa"
+            ></HeritageClause>
+            <HeritageClause
+                editing={editing}
+                node={list[1]}
+                key="bbb"
+            ></HeritageClause>
         </div>
     )
 }
