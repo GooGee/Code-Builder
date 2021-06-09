00e37e9f71ba165c6e7f968a94c948cefde03170
GooGee <mou-gj@live.com>
2021-06-09 17:22:24 +0800

    + StatementLine in ImportDeclaration.tsx

--- src/view/declaration/ImportDeclaration.tsx
+++ src/view/declaration/ImportDeclaration.tsx
@@ -1,6 +1,8 @@
 import React, { ReactElement } from 'react'
 import ts from 'typescript'
+import SourceFileMenuFactory from '../../helper/Menu/SourceFileMenuFactory'
 import ImportClause from '../block/ImportClause'
+import StatementLine from '../control/StatementLine'
 import Literal from '../expression/Literal'
 import Keyword from '../text/Keyword'
 import Modifierxx from '../text/Modifierxx'
@@ -11,12 +13,20 @@ interface Props {
 
 export default function ImportDeclaration({ node }: Props): ReactElement {
     return (
-        <div>
-            <Modifierxx list={node.modifiers}></Modifierxx>
-            <Keyword kind={node.kind}></Keyword>
-            <ImportClause node={node.importClause}></ImportClause>
-            <span className="keyword">from</span>
-            <Literal node={node.moduleSpecifier as any}></Literal>
-        </div>
+        <StatementLine
+            menuFactory={SourceFileMenuFactory(node)}
+            viewFactory={(editing) => (
+                <div>
+                    <Modifierxx list={node.modifiers}></Modifierxx>
+                    <Keyword kind={node.kind}></Keyword>
+                    <ImportClause
+                        editing={editing}
+                        node={node.importClause}
+                    ></ImportClause>
+                    <span className="keyword">from</span>
+                    <Literal node={node.moduleSpecifier as any}></Literal>
+                </div>
+            )}
+        ></StatementLine>
     )
 }
