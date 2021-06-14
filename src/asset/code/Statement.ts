04d5c53e469f8e34f88c9cfee0c1161862d78799
GooGee <mou-gj@live.com>
2021-06-14 17:14:54 +0800

    + code Statement.ts

---
+++ src/asset/code/Statement.ts
@@ -0,0 +1,53 @@
+do {
+    break
+} while (false)
+
+for (let index = 0; index < 0; index++) {
+    continue
+}
+
+const set: Set<string> = new Set()
+for (const key in set) {
+}
+
+const list: number[] = [1, 2]
+for (const iterator of list) {
+}
+
+list.find((item) => item === 0)
+list.forEach((item) => {
+    return
+})
+
+if (!true) {
+} else {
+}
+
+let literal: 'sss' | 'ccc'
+
+try {
+    throw new Error('Error')
+} catch (error) {
+} finally {
+}
+
+let value = 0
+switch (value) {
+    case 1:
+        break
+
+    case 2:
+    case 3:
+        break
+
+    default:
+        break
+}
+
+while (false) {
+    break
+}
+
+type ut = null | undefined | Array<string>
+
+export default ut
