eaf0cedab8f4077eb9704293f86e5a5cc14659f5
GooGee <mou-gj@live.com>
2021-06-14 16:05:55 +0800

    - createSystem in Worker.ts

--- src/model/TS/Worker.ts
+++ src/model/TS/Worker.ts
@@ -1,31 +1,12 @@
 import ts from 'typescript'
-import { createSystem } from '@typescript/vfs'
 import Host from './Host'
 
 export default class Worker {
-    readonly fs
-    readonly ls
-    readonly lsh
-
     constructor(
-        fileMap: Map<string, string>,
-        compilerOptions: ts.CompilerOptions,
-        files: string[],
-    ) {
-        this.fs = createSystem(fileMap)
-        const host = new Host(this.fs, compilerOptions, files)
-        this.lsh = host
-        // const {
-        //     languageServiceHost,
-        //     updateFile,
-        // } = createVirtualLanguageServiceHost(
-        //     this.fs,
-        //     Array.from(files.keys()),
-        //     compilerOptions,
-        //     ts,
-        // )
-        this.ls = ts.createLanguageService(host)
-    }
+        readonly fs: ts.System,
+        readonly lsh: Host,
+        readonly ls: ts.LanguageService,
+    ) {}
 
     getSourceFile(fileName: string) {
         return this.ls.getProgram()?.getSourceFile(fileName)
