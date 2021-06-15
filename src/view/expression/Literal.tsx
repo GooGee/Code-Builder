42caf393acba3f7863e2b1b077ea244d33a38b72
GooGee <mou-gj@live.com>
2021-06-15 15:19:28 +0800

    + replaceLiteral in Literal.tsx

--- src/view/expression/Literal.tsx
+++ src/view/expression/Literal.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement } from 'react'
 import ts from 'typescript'
+import { replaceLiteral } from '../../helper/Transformer/ExpressionTransformer'
+import Button from '../control/Button'
 
 interface Props {
     editing?: boolean
@@ -7,6 +9,22 @@ interface Props {
 }
 
 export default function Literal({ editing, node }: Props): ReactElement {
+    if (editing) {
+        return (
+            <Button
+                onClick={() => {
+                    const value = prompt('Enter a literal', node.text)
+                    if (value === null) {
+                        return
+                    }
+                    replaceLiteral(node, value)
+                }}
+            >
+                {node.text}
+            </Button>
+        )
+    }
+
     const text = ts.isStringLiteral(node) ? `"${node.text}"` : node.text
     return <span className="literal">{text}</span>
 }
