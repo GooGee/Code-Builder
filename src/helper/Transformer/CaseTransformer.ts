c1ce5a4b28df2e0a0d5780ea3296887d884d45ee
GooGee <mou-gj@live.com>
2021-06-16 14:23:40 +0800

    - getMutableClone in CaseTransformer.ts

--- src/helper/Transformer/CaseTransformer.ts
+++ src/helper/Transformer/CaseTransformer.ts
@@ -1,18 +1,13 @@
 import ts from 'typescript'
 import Transformer from './Transformer'
 
-function addNode(
-    parent: ts.CaseBlock,
-    at?: ts.CaseClause,
-) {
-    const item = ts.factory.createCaseClause(
-        ts.factory.createNull(),
-        [ts.factory.createBreakStatement()],
-    )
+function addNode(parent: ts.CaseBlock, at?: ts.CaseClause) {
+    const item = ts.factory.createCaseClause(ts.factory.createNull(), [
+        ts.factory.createBreakStatement(),
+    ])
     const list = Transformer.insert(parent.clauses, item, at)
-    const clone = ts.getMutableClone(parent) as any
-    clone.clauses = list
-    Transformer.replace(parent, clone)
+    const to = ts.factory.updateCaseBlock(parent, list)
+    Transformer.replace(parent, to)
 }
 
 export default {
