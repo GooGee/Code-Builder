041e2ff20ba58e52002bd9a5384cb3c67b158018
GooGee <mou-gj@live.com>
2021-06-23 15:34:55 +0800

    -+ LibraryMap.ts

--- src/asset/LibraryMap.ts
+++ src/asset/LibraryMap.ts
@@ -3,31 +3,46 @@
 import StringMap from '../model/StringMap'
 
 const LibraryMap: StringMap = {
-    'es6.d.ts': require('!raw-loader!typescript/lib/lib.es2015.d.ts').default,
-    'dom.d.ts': require('!raw-loader!typescript/lib/lib.dom.d.ts').default,
     'lib.es5.d.ts': require('!raw-loader!typescript/lib/lib.es5.d.ts').default,
-    'lib.es6.d.ts': require('!raw-loader!typescript/lib/lib.es2015.d.ts')
+    'lib.es6.d.ts': require('!raw-loader!typescript/lib/lib.es6.d.ts').default,
+    'lib.es2015.d.ts': require('!raw-loader!typescript/lib/lib.es2015.d.ts')
         .default,
-    'lib.es6.core.d.ts':
+    'lib.dom.d.ts': require('!raw-loader!typescript/lib/lib.dom.d.ts').default,
+    'lib.dom.iterable.d.ts':
+        require('!raw-loader!typescript/lib/lib.dom.iterable.d.ts').default,
+    'lib.webworker.importscripts.d.ts':
+        require('!raw-loader!typescript/lib/lib.webworker.importscripts.d.ts')
+            .default,
+    'lib.scripthost.d.ts':
+        require('!raw-loader!typescript/lib/lib.scripthost.d.ts').default,
+    'lib.es2015.core.d.ts':
         require('!raw-loader!typescript/lib/lib.es2015.core.d.ts').default,
-    'lib.es6.collection.d.ts':
+    'lib.es2015.collection.d.ts':
         require('!raw-loader!typescript/lib/lib.es2015.collection.d.ts')
             .default,
-    'lib.es6.generator.d.ts':
-        require('!raw-loader!typescript/lib/lib.es2015.generator.d.ts').default,
-    'lib.es6.promise.d.ts':
-        require('!raw-loader!typescript/lib/lib.es2015.promise.d.ts').default,
-    'lib.es6.iterable.d.ts':
+    'lib.es2015.iterable.d.ts':
         require('!raw-loader!typescript/lib/lib.es2015.iterable.d.ts').default,
-    'lib.es6.proxy.d.ts':
+    'lib.es2015.generator.d.ts':
+        require('!raw-loader!typescript/lib/lib.es2015.generator.d.ts').default,
+    'lib.es2015.promise.d.ts':
+        require('!raw-loader!typescript/lib/lib.es2015.promise.d.ts').default,
+    'lib.es2015.proxy.d.ts':
         require('!raw-loader!typescript/lib/lib.es2015.proxy.d.ts').default,
-    'lib.es6.reflect.d.ts':
+    'lib.es2015.reflect.d.ts':
         require('!raw-loader!typescript/lib/lib.es2015.reflect.d.ts').default,
-    'lib.es6.symbol.d.ts':
+    'lib.es2015.symbol.d.ts':
         require('!raw-loader!typescript/lib/lib.es2015.symbol.d.ts').default,
-    'lib.es6.symbol.wellknown.d.ts':
+    'lib.es2015.symbol.wellknown.d.ts':
         require('!raw-loader!typescript/lib/lib.es2015.symbol.wellknown.d.ts')
             .default,
+    'typescript.d.ts': require('!raw-loader!typescript/lib/typescript.d.ts')
+        .default,
 }
+
+Object.keys(LibraryMap).forEach((key) => {
+    if (key.includes('lib.')) {
+        LibraryMap[key.substring(4)] = LibraryMap[key]
+    }
+})
 
 export default LibraryMap
