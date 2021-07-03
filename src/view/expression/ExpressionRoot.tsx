6d768c85519ba9ec3b49a9aa8bf19514698e6103
GooGee <mou-gj@live.com>
2021-07-03 14:17:53 +0800

    + EditingView in ExpressionRoot.tsx

--- src/view/expression/ExpressionRoot.tsx
+++ src/view/expression/ExpressionRoot.tsx
@@ -1,11 +1,10 @@
 import React, { ReactElement } from 'react'
 import ts from 'typescript'
 import ExpressionMenuFactory from '../../helper/Menu/ExpressionMenuFactory'
-import MenuButton from '../control/MenuButton'
+import EditingView from '../control/EditingView'
 import Expression from './Expression'
 
 interface Props {
-    editing: boolean
     isLeft?: boolean
     node: ts.Expression | undefined
     parent: ts.Node
@@ -13,34 +12,25 @@ interface Props {
 }
 
 export default function ExpressionRoot({
-    editing,
     isLeft = false,
     node,
     parent,
     propertyName,
 }: Props): ReactElement | null {
-    if (editing) {
-        return (
-            <span>
-                <MenuButton
-                    visible={true}
-                    factory={ExpressionMenuFactory(
-                        parent,
-                        node,
-                        propertyName,
-                        isLeft,
-                    )}
-                ></MenuButton>
-                {node === undefined ? null : (
-                    <Expression editing={true} node={node}></Expression>
-                )}
-            </span>
-        )
-    }
-
-    if (node === undefined) {
-        return null
-    }
-
-    return <Expression editing={editing} node={node}></Expression>
+    return (
+        <EditingView
+            menuFactory={ExpressionMenuFactory(
+                parent,
+                node,
+                propertyName,
+                isLeft,
+            )}
+            viewFactory={(editing) => {
+                if (node === undefined) {
+                    return null
+                }
+                return <Expression editing={editing} node={node}></Expression>
+            }}
+        ></EditingView>
+    )
 }
