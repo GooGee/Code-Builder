38bbed7fe9f0777ad2a09be5544ae2c34f003df2
GooGee <mou-gj@live.com>
2021-07-05 15:42:15 +0800

    + AssignTokenMenuFactory in TokenMenuFactory.ts

--- src/helper/Menu/TokenMenuFactory.ts
+++ src/helper/Menu/TokenMenuFactory.ts
@@ -2,6 +2,23 @@ import ts from 'typescript'
 import Transformer from '../Transformer/Transformer'
 import MenuFactory from './MenuFactory'
 
+const AssignTokenList = [
+    ts.SyntaxKind.AmpersandEqualsToken,
+    ts.SyntaxKind.BarEqualsToken,
+    ts.SyntaxKind.AsteriskEqualsToken,
+    ts.SyntaxKind.EqualsToken,
+    ts.SyntaxKind.MinusEqualsToken,
+    ts.SyntaxKind.PlusEqualsToken,
+    ts.SyntaxKind.SlashEqualsToken,
+]
+
+export function AssignTokenMenuFactory(token: ts.BinaryOperatorToken) {
+    return () => {
+        console.log('AssignTokenMenuFactory')
+        return makeAllMenu(token, AssignTokenList)
+    }
+}
+
 export function makeMenu(parent: ts.BinaryExpression, kind: ts.SyntaxKind) {
     const text = ts.tokenToString(kind) ?? ''
     const menu = MenuFactory.makeMenu(text, () => {
