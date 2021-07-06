c78389e854baf3eaefe4773d0568cdc4c8daa5d6
GooGee <mou-gj@live.com>
2021-07-06 15:20:24 +0800

    - editing in ComputeToken.tsx

--- src/view/text/ComputeToken.tsx
+++ src/view/text/ComputeToken.tsx
@@ -5,20 +5,13 @@ import MenuButton from '../control/MenuButton'
 import Token from './Token'
 
 interface Props {
-    editing: boolean
     token: ts.BinaryOperatorToken
 }
 
-export default function ComputeToken({ editing, token }: Props): ReactElement {
-    if (editing) {
-        return (
-            <MenuButton
-                factory={ComputeTokenMenuFactory(token)}
-                text={ts.tokenToString(token.kind)}
-                visible={true}
-            ></MenuButton>
-        )
-    }
-
-    return <Token kind={token.kind}></Token>
+export default function ComputeToken({ token }: Props): ReactElement {
+    return (
+        <MenuButton factory={ComputeTokenMenuFactory(token)} visible={true}>
+            <Token kind={token.kind}></Token>
+        </MenuButton>
+    )
 }
