4f56140b5ffe41b1dcaf4f97bdc59317e71995ba
GooGee <mou-gj@live.com>
2021-07-06 15:06:42 +0800

    + EditingView in VariableStatement.tsx

--- src/view/statement/VariableStatement.tsx
+++ src/view/statement/VariableStatement.tsx
@@ -1,8 +1,10 @@
 import React, { ReactElement } from 'react'
 import ts from 'typescript'
 import StatementMenuFactory from '../../helper/Menu/StatementMenuFactory'
+import EditingView from '../control/EditingView'
 import MenuButton from '../control/MenuButton'
-import VDLView from '../declaration/VDLView'
+import VariableDeclarationxx from '../declaration/VariableDeclarationxx'
+import Flag from '../text/Flag'
 import Modifierxx from '../text/Modifierxx'
 
 interface Props {
@@ -12,13 +14,20 @@ interface Props {
 export default function VariableStatement({ node }: Props): ReactElement {
     return (
         <span>
+            <Modifierxx list={node.modifiers}></Modifierxx>
             <MenuButton
                 factory={StatementMenuFactory(node.parent as any, node)}
             >
-                <span className="keyword">define </span>
+                <Flag node={node.declarationList}></Flag>
             </MenuButton>
-            <Modifierxx list={node.modifiers}></Modifierxx>
-            <VDLView node={node.declarationList}></VDLView>
+            <EditingView
+                viewFactory={(editing) => (
+                    <VariableDeclarationxx
+                        editing={editing}
+                        list={node.declarationList.declarations}
+                    ></VariableDeclarationxx>
+                )}
+            ></EditingView>
         </span>
     )
 }
