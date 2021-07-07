4a5155d2d5d59c434b50adc9353c39a8e7c6001b
GooGee <mou-gj@live.com>
2021-07-07 14:58:01 +0800

    - getxx in ParameterDeclarationxx.tsx

--- src/view/declaration/ParameterDeclarationxx.tsx
+++ src/view/declaration/ParameterDeclarationxx.tsx
@@ -13,19 +13,18 @@ export default function ParameterDeclarationxx({
     list,
 }: Props): ReactElement {
     const uk = UniqueKey()
-    function getxx() {
-        if (list.length === 0) {
-            return null
-        }
-        return list.map((item) => {
-            return (
-                <ParameterDeclaration
-                    node={item}
-                    key={uk()}
-                ></ParameterDeclaration>
+    return (
+        <span>
+            (
+            {list.length === 0
+                ? null
+                : list.map((item) => (
+                      <ParameterDeclaration
+                          key={uk()}
+                          node={item}
+                      ></ParameterDeclaration>
+                  ))}
             )
-        })
-    }
-
-    return <span>({getxx()})</span>
+        </span>
+    )
 }
