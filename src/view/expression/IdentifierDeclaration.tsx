63afc1ef61b27e336415b06c5780c6a5bff9a29b
GooGee <mou-gj@live.com>
2021-07-12 16:03:47 +0800

    - Button in IdentifierDeclaration.tsx

--- src/view/expression/IdentifierDeclaration.tsx
+++ src/view/expression/IdentifierDeclaration.tsx
@@ -2,39 +2,30 @@ import React, { ReactElement, useContext } from 'react'
 import ts from 'typescript'
 import refactor from '../../helper/refactor'
 import SourceFileContext from '../context/SourceFileContext'
-import Button from '../control/Button'
 import Identifier from './Identifier'
 
 interface Props {
-    editing: boolean
     node: ts.Identifier
 }
 
-export default function IdentifierDeclaration({
-    editing,
-    node,
-}: Props): ReactElement {
+export default function IdentifierDeclaration({ node }: Props): ReactElement {
     const context = useContext(SourceFileContext)
-    if (editing) {
-        return (
-            <Button
-                onClick={() => {
-                    const value = prompt('Enter a name', node.text)
-                    if (value === null) {
-                        return
-                    }
-                    try {
-                        refactor(node, value)
-                        context.update!()
-                    } catch (error) {
-                        alert(error.message)
-                    }
-                }}
-            >
-                {node.text}
-            </Button>
-        )
-    }
-
-    return <Identifier node={node}></Identifier>
+    return (
+        <span
+            onClick={() => {
+                const value = prompt('Enter a name', node.text)
+                if (value === null) {
+                    return
+                }
+                try {
+                    refactor(node, value)
+                    context.update!()
+                } catch (error) {
+                    alert(error.message)
+                }
+            }}
+        >
+            <Identifier node={node}></Identifier>
+        </span>
+    )
 }
