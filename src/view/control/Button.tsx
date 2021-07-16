7ab4dae2433c888dc14bbecc37029cdaaff14763
GooGee <mou-gj@live.com>
2021-07-16 14:42:18 +0800

    + text color in Button.tsx

--- src/view/control/Button.tsx
+++ src/view/control/Button.tsx
@@ -13,6 +13,7 @@ export default function Button({
 }: Props): ReactElement {
     const list = [
         `border-${color}-300`,
+        `text-${color}-500`,
         `hover:bg-${color}-200`,
         'cursor-pointer px-2 mr-1 border rounded-md',
     ]
