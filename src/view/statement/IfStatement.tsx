35ea4de2938e8c8ea6f729cd6264c8cfc51b3c14
GooGee <mou-gj@live.com>
2021-07-16 16:44:51 +0800

    + visible for else in IfStatement.tsx

--- src/view/statement/IfStatement.tsx
+++ src/view/statement/IfStatement.tsx
@@ -1,16 +1,19 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import ts from 'typescript'
 import StatementMenuFactory from '../../helper/Menu/StatementMenuFactory'
 import Block from '../block/Block'
 import MenuButton from '../control/MenuButton'
 import ExpressionRoot from '../expression/ExpressionRoot'
 import Keyword from '../text/Keyword'
+import PointerText from '../text/PointerText'
 
 interface Props {
     node: ts.IfStatement
 }
 
 export default function IfStatement({ node }: Props): ReactElement {
+    const block = node.elseStatement as ts.Block
+    const [visible, setVisible] = useState(block.statements.length > 0)
     return (
         <span>
             <MenuButton
@@ -24,8 +27,21 @@ export default function IfStatement({ node }: Props): ReactElement {
             ></ExpressionRoot>
             <br />
             <Block node={node.thenStatement as any}></Block>
-            <div className="keyword">else</div>
-            <Block node={node.elseStatement as any}></Block>
+            <PointerText
+                onClick={() => {
+                    if (block.statements.length) {
+                        return
+                    }
+                    setVisible(!visible)
+                }}
+            >
+                {' else '}
+            </PointerText>
+            {visible === false ? (
+                '{}'
+            ) : (
+                <Block node={node.elseStatement as any}></Block>
+            )}
         </span>
     )
 }
