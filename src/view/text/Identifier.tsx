4d718ab5a9af059eff79e5b5b15fdc1d58cff564
GooGee <mou-gj@live.com>
2021-07-23 16:50:09 +0800

    + ErrorTip in Identifier.tsx

--- src/view/text/Identifier.tsx
+++ src/view/text/Identifier.tsx
@@ -1,10 +1,19 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useContext } from 'react'
 import ts from 'typescript'
+import SourceFileContext from '../context/SourceFileContext'
+import ErrorTip from '../control/ErrorTip'
 
 interface Props {
     node: ts.Identifier
 }
 
 export default function Identifier({ node }: Props): ReactElement {
+    const context = useContext(SourceFileContext)
+    if (context.state?.worker.diagnosticMap.has(node.getStart())) {
+        const diagnostic = context.state?.worker.diagnosticMap.get(
+            node.getStart(),
+        )
+        return <ErrorTip text={node.text} diagnostic={diagnostic!}></ErrorTip>
+    }
     return <span className="identifier cursor-pointer">{node.text}</span>
 }
