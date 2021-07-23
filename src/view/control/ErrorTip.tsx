1033daa69b17bd8ec5499b9c518076e755f430d6
GooGee <mou-gj@live.com>
2021-07-23 16:49:04 +0800

    + ErrorTip.tsx

---
+++ src/view/control/ErrorTip.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement } from 'react'
+import Popup from 'reactjs-popup'
+import ts from 'typescript'
+
+interface Props {
+    text: string
+    diagnostic: ts.Diagnostic
+}
+
+export default function ErrorTip({ text, diagnostic }: Props): ReactElement {
+    return (
+        <Popup
+            trigger={<span className="error cursor-pointer">{text}</span>}
+            closeOnDocumentClick
+            position="bottom left"
+        >
+            <span>
+                {typeof diagnostic.messageText === 'string'
+                    ? diagnostic.messageText
+                    : diagnostic.messageText.messageText}
+            </span>
+        </Popup>
+    )
+}
