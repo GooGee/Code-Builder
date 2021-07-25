37259c63394fc7ff5aba0777a2ea3092f2cf68f5
GooGee <mou-gj@live.com>
2021-07-25 14:35:43 +0800

    + ErrorTip in Literal.tsx

--- src/view/text/Literal.tsx
+++ src/view/text/Literal.tsx
@@ -1,11 +1,20 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useContext } from 'react'
 import ts from 'typescript'
+import SourceFileContext from '../context/SourceFileContext'
+import ErrorTip from '../control/ErrorTip'
 
 interface Props {
     node: ts.NumericLiteral | ts.StringLiteral
 }
 
 export default function Literal({ node }: Props): ReactElement {
+    const context = useContext(SourceFileContext)
     const text = ts.isStringLiteral(node) ? `"${node.text}"` : node.text
+    if (context.state?.worker.diagnosticMap.has(node.getStart())) {
+        const diagnostic = context.state?.worker.diagnosticMap.get(
+            node.getStart(),
+        )
+        return <ErrorTip text={text} diagnostic={diagnostic!}></ErrorTip>
+    }
     return <span className="literal cursor-pointer">{text}</span>
 }
