5c321488da63c12fef1caf0d0d6c00f518ea16b0
GooGee <mou-gj@live.com>
2021-07-26 16:55:42 +0800

    + TypeArgumentTable.tsx

---
+++ src/view/type/TypeArgumentTable.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement } from 'react'
+import ts from 'typescript'
+import UniqueKey from '../../helper/UniqueKey'
+import TypeRoot from './TypeRoot'
+
+interface Props {
+    children: ReactElement
+    list: ts.NodeArray<ts.TypeNode>
+}
+
+export default function TypeArgumentTable({
+    children,
+    list,
+}: Props): ReactElement {
+    const uk = UniqueKey()
+    return (
+        <table className="table ml-11">
+            <thead>
+                <tr>
+                    <th className="text-right">{children}</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {list.map((node) => (
+                    <tr key={uk()}>
+                        <td>
+                            <TypeRoot
+                                node={node}
+                                parent={node.parent}
+                            ></TypeRoot>
+                        </td>
+                        <td></td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
