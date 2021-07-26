ef67b6ba5b7d1f6a424c7f9f3bbe1842dbe0f898
GooGee <mou-gj@live.com>
2021-07-26 13:34:13 +0800

    - NameValue in ParameterDeclaration.tsx

--- src/view/declaration/ParameterDeclaration.tsx
+++ src/view/declaration/ParameterDeclaration.tsx
@@ -1,11 +1,10 @@
 import React, { ReactElement } from 'react'
 import ts from 'typescript'
-import NameValue from './NameValue'
 
 interface Props {
     node: ts.ParameterDeclaration
 }
 
 export default function ParameterDeclaration({ node }: Props): ReactElement {
-    return <NameValue node={node}></NameValue>
+    return <span className="cursor-pointer">{node.getText()}</span>
 }
