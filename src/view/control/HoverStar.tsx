a5d7bc29c2e415efd0fe790f692d5d579b4b3457
GooGee <mou-gj@live.com>
2021-07-29 14:50:30 +0800

    -+ CSS in HoverStar.tsx

--- src/view/control/HoverStar.tsx
+++ src/view/control/HoverStar.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function HoverStar({ text = '*' }: Props): ReactElement {
     return (
-        <span className="cursor-pointer px-2 text-gray-50 hover:text-red-500">
+        <span className="cursor-pointer px-2 text-white hover:text-green-500">
             {text}
         </span>
     )
