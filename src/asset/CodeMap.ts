e32e266fea09fba6f6b52d0aed36f1eb47a0d445
GooGee <mou-gj@live.com>
2021-08-04 16:46:19 +0800

    -+ code in CodeMap.ts

--- src/asset/CodeMap.ts
+++ src/asset/CodeMap.ts
@@ -3,5 +3,5 @@
 export default {
     declaration: require('!raw-loader!./code/Declaration.ts').default,
     statement: require('!raw-loader!./code/Statement.ts').default,
-    test: 'const list = [1, 2, 3]\n',
+    test: 'const list: Array<number> = [1, 2, 3]\nlet sum: number = 0\n',
 }
