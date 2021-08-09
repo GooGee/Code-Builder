f5a92718670d40d2b710417ce1c6ec9415213b5b
GooGee <mou-gj@live.com>
2021-08-09 16:08:02 +0800

    + getVariableList in Finder.ts

--- src/helper/Finder.ts
+++ src/helper/Finder.ts
@@ -16,8 +16,76 @@ function getParameterList(node: ts.Node) {
     return list
 }
 
+function findVariable(
+    node: ts.Node,
+    child: ts.Node,
+    list: Array<ts.VariableDeclaration>,
+) {
+    if (ts.isSourceFile(node)) {
+        find(node.statements)
+        return
+    }
+    findVariable(node.parent, node, list)
+    if (ts.isBlock(node)) {
+        find(node.statements)
+        return
+    }
+    if (ts.isForInStatement(node)) {
+        if (ts.isVariableDeclarationList(node.initializer)) {
+            add(node.initializer)
+        }
+        return
+    }
+    if (ts.isForOfStatement(node)) {
+        if (ts.isVariableDeclarationList(node.initializer)) {
+            add(node.initializer)
+        }
+        return
+    }
+    if (ts.isForStatement(node)) {
+        if (node.initializer) {
+            if (ts.isVariableDeclarationList(node.initializer)) {
+                add(node.initializer)
+            }
+        }
+        return
+    }
+    if (ts.isTryStatement(node)) {
+        if (node.catchClause) {
+            if (node.catchClause.variableDeclaration) {
+                list.push(node.catchClause.variableDeclaration)
+            }
+        }
+        return
+    }
+
+    function add(vdl: ts.VariableDeclarationList) {
+        vdl.declarations.forEach((item) => {
+            list.push(item)
+        })
+    }
+    function find(statements: ts.NodeArray<ts.Statement>) {
+        statements.every((statement) => {
+            if (Object.is(statement, child)) {
+                return false
+            }
+            if (ts.isVariableStatement(statement)) {
+                add(statement.declarationList)
+            }
+            return true
+        })
+    }
+}
+
+function getVariableList(node: ts.Node) {
+    const list = new Array<ts.VariableDeclaration>()
+    findVariable(node, node, list)
+    return list
+}
+
 const Finder = {
     getParameterList,
+    getVariableList,
 }
 
 export default Finder
--- src/helper/Menu/ExpressionMenuFactory.ts
+++ src/helper/Menu/ExpressionMenuFactory.ts
@@ -1,6 +1,5 @@
 import ts from 'typescript'
 import CommonTypeList from '../../asset/CommonTypeList'
-import state from '../../state'
 import Finder from '../Finder'
 import LiteralTransformer from '../Transformer/LiteralTransformer'
 import Transformer from '../Transformer/Transformer'
@@ -121,10 +120,10 @@ function makeVariableMenu(
     old?: ts.Expression,
 ) {
     const menu = MenuFactory.makeMenu('Variable')
-    state.worker.checker.getVariableList(parent).forEach((item) => {
+    Finder.getVariableList(parent).forEach((item) => {
         menu.list.push(
-            MenuFactory.makeMenu(item.name, () => {
-                const node = ts.factory.createIdentifier(item.name)
+            MenuFactory.makeMenu(item.name.getText(), () => {
+                const node = ts.factory.createIdentifier(item.name.getText())
                 Transformer.transform(node, parent, propertyName, old)
             }),
         )
