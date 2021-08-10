c45168bbde1c77dcbcee8ec1f4cc928acc7246f5
GooGee <mou-gj@live.com>
2021-08-10 15:48:15 +0800

    + ExpressionRootEdit in Assign.tsx

--- src/view/expression/Assign.tsx
+++ src/view/expression/Assign.tsx
@@ -1,27 +1,49 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import ts from 'typescript'
 import AssignToken from '../text/AssignToken'
 import ExpressionRoot from './ExpressionRoot'
+import ExpressionRootEdit from './ExpressionRootEdit'
 
 interface Props {
     node: ts.BinaryExpression
 }
 
 export default function Assign({ node }: Props): ReactElement {
+    const [editing, setEditing] = useState(false)
     return (
-        <span>
-            <ExpressionRoot
-                isLeft={true}
-                node={node.left}
-                parent={node}
-                propertyName="left"
-            ></ExpressionRoot>{' '}
+        <span
+            onMouseEnter={(event) => setEditing(true)}
+            onMouseLeave={(event) => setEditing(false)}
+        >
+            {editing ? (
+                <ExpressionRootEdit
+                    isLeft={true}
+                    node={node.left}
+                    parent={node}
+                    propertyName="left"
+                ></ExpressionRootEdit>
+            ) : (
+                <ExpressionRoot
+                    isLeft={true}
+                    node={node.left}
+                    parent={node}
+                    propertyName="left"
+                ></ExpressionRoot>
+            )}{' '}
             <AssignToken token={node.operatorToken}></AssignToken>{' '}
-            <ExpressionRoot
-                node={node.right}
-                parent={node}
-                propertyName="right"
-            ></ExpressionRoot>
+            {editing ? (
+                <ExpressionRootEdit
+                    node={node.right}
+                    parent={node}
+                    propertyName="right"
+                ></ExpressionRootEdit>
+            ) : (
+                <ExpressionRoot
+                    node={node.right}
+                    parent={node}
+                    propertyName="right"
+                ></ExpressionRoot>
+            )}
         </span>
     )
 }
