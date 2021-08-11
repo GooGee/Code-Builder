97190da6d9b9e9f4a7f2e8e02655977fe10f04bb
GooGee <mou-gj@live.com>
2021-08-11 13:12:20 +0800

    + StatementMenuFactory in InterfaceDeclaration.tsx

--- src/view/declaration/InterfaceDeclaration.tsx
+++ src/view/declaration/InterfaceDeclaration.tsx
@@ -1,7 +1,9 @@
 import React, { ReactElement } from 'react'
 import ts from 'typescript'
 import InterfaceMenuFactory from '../../helper/Menu/InterfaceMenuFactory'
+import StatementMenuFactory from '../../helper/Menu/StatementMenuFactory'
 import UniqueKey from '../../helper/UniqueKey'
+import HoverButton from '../control/HoverButton'
 import MenuButton from '../control/MenuButton'
 import IdentifierDeclaration from '../expression/IdentifierDeclaration'
 import Keyword from '../text/Keyword'
@@ -19,20 +21,18 @@ export default function InterfaceDeclaration({ node }: Props): ReactElement {
     const uk = UniqueKey()
     return (
         <div>
-            <span>
-                <Modifierxx list={node.modifiers}></Modifierxx>{' '}
+            <Modifierxx list={node.modifiers}></Modifierxx>{' '}
+            <MenuButton
+                factory={StatementMenuFactory(node.parent as any, node)}
+            >
                 <Keyword kind={node.kind} suffix=" "></Keyword>
-                <IdentifierDeclaration node={node.name}></IdentifierDeclaration>
-                <TypeParameterDeclarationxx
-                    list={node.typeParameters}
-                ></TypeParameterDeclarationxx>
-                <Heritagexx
-                    list={node.heritageClauses}
-                    parent={node}
-                ></Heritagexx>
-            </span>
-
-            {'{'}
+            </MenuButton>
+            <IdentifierDeclaration node={node.name}></IdentifierDeclaration>
+            <TypeParameterDeclarationxx
+                list={node.typeParameters}
+            ></TypeParameterDeclarationxx>
+            <Heritagexx list={node.heritageClauses} parent={node}></Heritagexx>
+            {' {'}
             <div className="pl-11">
                 {node.members.map((item) =>
                     ts.isMethodSignature(item) ? (
@@ -48,7 +48,7 @@ export default function InterfaceDeclaration({ node }: Props): ReactElement {
                     ) : null,
                 )}
                 <MenuButton factory={InterfaceMenuFactory(node)}>
-                    <span className="cursor-pointer px-2 py-1 mr-1">+</span>
+                    <HoverButton>+</HoverButton>
                 </MenuButton>
             </div>
             {'}'}
