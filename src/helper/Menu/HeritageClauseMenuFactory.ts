be78680e1e63844cdafef85709141ebe6cbba5fd
GooGee <mou-gj@live.com>
2021-08-13 15:19:47 +0800

    + makeClassHeritageClause in HeritageClauseMenuFactory.ts

--- src/helper/Menu/HeritageClauseMenuFactory.ts
+++ src/helper/Menu/HeritageClauseMenuFactory.ts
@@ -1,6 +1,46 @@
 import ts from 'typescript'
+import Menu from '../../model/Menu'
 import Transformer from '../Transformer/Transformer'
 import MenuFactory from './MenuFactory'
+
+function makeClassHeritageClause(menu: Menu, parent: ts.ClassLikeDeclaration) {
+    if (parent.heritageClauses) {
+        if (parent.heritageClauses.length === 2) {
+            return
+        }
+        let title = '+ extends'
+        const list: ts.HeritageClause[] = []
+        const hc = parent.heritageClauses[0]
+        if (hc.token === ts.SyntaxKind.ExtendsKeyword) {
+            title = '+ implements'
+            list.push(hc)
+            list.push(makeHeritageClause(ts.SyntaxKind.ImplementsKeyword))
+        } else {
+            list.push(makeHeritageClause(ts.SyntaxKind.ExtendsKeyword))
+            list.push(hc)
+        }
+        menu.list.push(
+            MenuFactory.makeMenu(title, () => {
+                Transformer.setProperty(
+                    parent,
+                    ts.factory.createNodeArray(list),
+                    'heritageClauses',
+                )
+            }),
+        )
+        return
+    }
+
+    menu.list.push(makeExtendsMenu(parent))
+    const list = ts.factory.createNodeArray([
+        makeHeritageClause(ts.SyntaxKind.ImplementsKeyword),
+    ])
+    menu.list.push(
+        MenuFactory.makeMenu('+ implements', () => {
+            Transformer.setProperty(parent, list, 'heritageClauses')
+        }),
+    )
+}
 
 function makeExtendsMenu(
     parent: ts.ClassLikeDeclaration | ts.InterfaceDeclaration,
@@ -45,6 +85,7 @@ export default function HeritageClauseMenuFactory(
             return menu
         }
 
+        makeClassHeritageClause(menu, parent)
         return menu
     }
 }
