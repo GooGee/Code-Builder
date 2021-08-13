3d2eaad9b00281dff72515e3a04cdf2d230d94bb
GooGee <mou-gj@live.com>
2021-08-13 15:08:22 +0800

    + HeritageClauseMenuFactory in Heritagexx.tsx

--- src/view/declaration/Heritagexx.tsx
+++ src/view/declaration/Heritagexx.tsx
@@ -1,17 +1,32 @@
 import React, { ReactElement } from 'react'
 import ts from 'typescript'
+import HeritageClauseMenuFactory from '../../helper/Menu/HeritageClauseMenuFactory'
 import UniqueKey from '../../helper/UniqueKey'
 import HeritageClause from '../block/HeritageClause'
+import HoverButton from '../control/HoverButton'
+import MenuButton from '../control/MenuButton'
 
 interface Props {
+    editing: boolean
     list: ts.NodeArray<ts.HeritageClause> | undefined
     parent: ts.ClassDeclaration | ts.InterfaceDeclaration
 }
 
 export default function Heritagexx({
+    editing,
     list,
     parent,
 }: Props): ReactElement | null {
+    if (editing) {
+        if (list === undefined || list.length === 0) {
+            return (
+                <MenuButton factory={HeritageClauseMenuFactory(parent)}>
+                    <HoverButton>+</HoverButton>
+                </MenuButton>
+            )
+        }
+    }
+
     if (list === undefined) {
         return null
     }
