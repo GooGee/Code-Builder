c55dadc296e4de4163af6a9299f460ad852a80d0
GooGee <mou-gj@live.com>
2021-08-13 15:41:18 +0800

    + TypeRoot in HeritageClause.tsx

--- src/view/block/HeritageClause.tsx
+++ src/view/block/HeritageClause.tsx
@@ -4,18 +4,35 @@ import HeritageClauseMenuFactory from '../../helper/Menu/HeritageClauseMenuFacto
 import MenuButton from '../control/MenuButton'
 import Keyword from '../text/Keyword'
 import TypeArrayBox from '../type/TypeArrayBox'
+import TypeRoot from '../type/TypeRoot'
 
 interface Props {
     node: ts.HeritageClause
 }
 
 export default function HeritageClause({ node }: Props): ReactElement {
+    function getTypes() {
+        const parent = node.parent
+        if (ts.isClassLike(parent)) {
+            if (node.token === ts.SyntaxKind.ExtendsKeyword) {
+                return (
+                    <TypeRoot
+                        node={node.types[0]}
+                        parent={parent}
+                        required={true}
+                    ></TypeRoot>
+                )
+            }
+        }
+        return <TypeArrayBox list={node.types} parent={node}></TypeArrayBox>
+    }
+
     return (
         <div>
             <MenuButton factory={HeritageClauseMenuFactory(node.parent, node)}>
                 <Keyword kind={node.token} suffix=" "></Keyword>
             </MenuButton>
-            <TypeArrayBox list={node.types} parent={node}></TypeArrayBox>
+            {getTypes()}
         </div>
     )
 }
