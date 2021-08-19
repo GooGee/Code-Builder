5630c38386abea0a908e9f092c0c90db66fd0707
GooGee <mou-gj@live.com>
2021-08-19 15:49:10 +0800

    + TypeParameterBox in TypeParameterDeclarationxx.tsx

--- src/view/declaration/TypeParameterDeclarationxx.tsx
+++ src/view/declaration/TypeParameterDeclarationxx.tsx
@@ -1,15 +1,55 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import ts from 'typescript'
-import UniqueKey from '../../helper/UniqueKey'
-import TypeParameterDeclaration from './TypeParameterDeclaration'
+import Button from '../control/Button'
+import HoverButton from '../control/HoverButton'
+import TypeParameterBox from './TypeParameterBox'
 
 interface Props {
+    hovering: boolean
     list: ts.NodeArray<ts.TypeParameterDeclaration> | undefined
+    parent:
+        | ts.ClassDeclaration
+        | ts.InterfaceDeclaration
+        | ts.FunctionDeclaration
+        | ts.MethodDeclaration
+        | ts.MethodSignature
 }
 
 export default function TypeParameterDeclarationxx({
+    hovering,
     list,
+    parent,
 }: Props): ReactElement | null {
+    const [editing, setEditing] = useState(false)
+    if (editing) {
+        return (
+            <TypeParameterBox list={list} parent={parent}>
+                <Button onClick={() => setEditing(false)}>x</Button>
+            </TypeParameterBox>
+        )
+    }
+
+    if (hovering) {
+        return (
+            <span
+                onClick={(event) => {
+                    event.stopPropagation()
+                    setEditing(true)
+                }}
+            >
+                <HoverButton>
+                    <span>
+                        &lt;
+                        {list
+                            ? list.map((item) => item.getText()).join(', ')
+                            : ''}
+                        &gt;
+                    </span>
+                </HoverButton>{' '}
+            </span>
+        )
+    }
+
     if (list === undefined) {
         return null
     }
@@ -17,21 +57,5 @@ export default function TypeParameterDeclarationxx({
         return null
     }
 
-    const uk = UniqueKey()
-    return (
-        <span>
-            &lt;
-            {list
-                .map((item) => (
-                    <TypeParameterDeclaration
-                        node={item}
-                        key={uk()}
-                    ></TypeParameterDeclaration>
-                ))
-                .reduce((previousValue, currentValue): any => {
-                    return [previousValue, ', ', currentValue]
-                })}
-            &gt;
-        </span>
-    )
+    return <span>&lt;{list.map((item) => item.getText()).join(', ')}&gt;</span>
 }
