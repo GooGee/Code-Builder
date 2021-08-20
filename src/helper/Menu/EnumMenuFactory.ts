475426b1a47381564f7605b79a03648cb7ae5f6d
GooGee <mou-gj@live.com>
2021-08-20 13:50:35 +0800

    - Refactor in EnumMenuFactory.ts

--- src/helper/Menu/EnumMenuFactory.ts
+++ src/helper/Menu/EnumMenuFactory.ts
@@ -1,8 +1,6 @@
 import ts from 'typescript'
 import * as DeclarationFactory from '../Factory/DeclarationFactory'
-import InputTool from '../InputTool'
 import EnumTransformer from '../Transformer/EnumTransformer'
-import Transformer from '../Transformer/Transformer'
 import { makeName } from './ClassMenuFactory'
 import MenuFactory from './MenuFactory'
 
@@ -15,33 +13,6 @@ export default function EnumMenuFactory(
         const menu = MenuFactory.makeMenu('')
         if (at !== undefined) {
             MenuFactory.addDelete(menu, at)
-            MenuFactory.addSeparator(menu)
-
-            menu.list.push(
-                MenuFactory.makeMenu('Refactor', () => {
-                    try {
-                        const text = InputTool.inputName(
-                            undefined,
-                            at.name.getText(),
-                        )
-                        if (text === null) {
-                            return
-                        }
-                        if (text === at.name.getText()) {
-                            return
-                        }
-
-                        const item = ts.factory.createIdentifier(text)
-                        Transformer.replace(at.name, item)
-                    } catch (error) {
-                        if (error.message) {
-                            window.alert(error.message)
-                        } else {
-                            window.alert(error)
-                        }
-                    }
-                }),
-            )
             MenuFactory.addSeparator(menu)
         }
 
