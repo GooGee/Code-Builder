a827da76bf409c3b5a6e6f85de710d10b69b6fb5
GooGee <mou-gj@live.com>
2021-08-30 15:16:19 +0800

    + GitHub link in App.tsx

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -12,6 +12,16 @@ export default function App({ state }: Props): ReactElement {
             id="MenuBoundary"
             className="xl:flex flex-nowrap font-mono text-xl"
         >
+            <a
+                className="fixed top-2 right-11"
+                href="https://github.com/GooGee/Code-Builder"
+            >
+                <img
+                    alt="GitHub"
+                    src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                    width="33"
+                ></img>
+            </a>
             <Builder state={state}></Builder>
             <hr />
         </div>
